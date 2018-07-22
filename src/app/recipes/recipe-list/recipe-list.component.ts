feedee7541b3ae27a8f93734ab6f6526b3279dd9
sumanth0304 <sumanth0304@gmail.com>
2018-07-22 16:33:59 -0400

    added ngfor to display list

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -8,7 +8,9 @@ import { Recipe } from "../recipe.model";
 })
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [
-    new Recipe("test1","this is the sample recipe1","https://food.fnr.sndimg.com/content/dam/images/food/fullset/2010/4/14/0/0046750F4_Wild-Rice-Pilaf-with-Nuts-and-Lemon_s4x3.jpg.rend.hgtvcom.616.462.suffix/1371591495859.jpeg")
+    new Recipe("test1","this is the sample recipe1","http://pngimg.com/uploads/fries/fries_PNG22.png"),
+    new Recipe("test1","this is the sample recipe1","http://pngimg.com/uploads/fries/fries_PNG22.png")
+
   ];
 
   constructor() { }
