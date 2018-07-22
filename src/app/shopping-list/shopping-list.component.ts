2e4baa3663b87b96af07613b295ce806f273aadd
sumanth0304 <sumanth0304@gmail.com>
2018-07-22 18:24:42 -0400

    added elements to sholistl

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,12 +1,17 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Ingredient } from "../shared/ingredient.model";
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
-
+  ingredients:Ingredient[] = [
+    new Ingredient("potato",12),
+    new Ingredient("sugar",8),
+    new Ingredient("chillies",10),
+    new Ingredient("tomatoes",9)
+  ];
   constructor() { }
 
   ngOnInit() {
