836c6551a4e7d9725d02260360dac240eb1b28fe
sumanth0304 <sumanth0304@gmail.com>
2018-07-22 15:48:49 -0400

    added content to the Recipe model

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from "../recipe.model";
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe("test1","this is the sample recipe1","https://food.fnr.sndimg.com/content/dam/images/food/fullset/2010/4/14/0/0046750F4_Wild-Rice-Pilaf-with-Nuts-and-Lemon_s4x3.jpg.rend.hgtvcom.616.462.suffix/1371591495859.jpeg")
+  ];
 
   constructor() { }
 
